20531444dafb869ec4bfb40ad22bc644312fb086
Matija Marohnić <matija.marohnic@gmail.com>
2016-01-07 22:24:19 +0100

    Make import paths local where convenient

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createHistory } from 'history';
 import { syncReduxAndRouter } from 'redux-simple-router';
-import configureStore from 'store/configureStore';
-import Root from 'containers/Root';
+import configureStore from './store/configureStore';
+import Root from './containers/Root';
 import 'bootstrap/scss/bootstrap-reboot.scss';
 
 const history = createHistory();
