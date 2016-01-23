627d1bd22543fd2ce2b85c6b37023e2f5a0e5b0b
Matija Marohnić <matija.marohnic@gmail.com>
2016-01-23 21:57:00 +0100

    Convert MeetingForm into a stateless component

--- src/components/MeetingForm.js
+++ src/components/MeetingForm.js
@@ -1,51 +1,47 @@
 import React, { PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 
-class MeetingForm extends React.Component {
-  static propTypes = {
-    fields: PropTypes.object.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-  };
+export const MeetingForm = (props) => {
+  const {
+    fields: { date },
+    handleSubmit,
+  } = props;
 
-  render() {
-    const {
-      fields: { date },
-      handleSubmit,
-    } = this.props;
-
-    return (
-      <form className="form-inline m-b-1" onSubmit={handleSubmit}>
-        <fieldset className="form-group">
-          <label
-            className="sr-only"
-            htmlFor="meetingDate"
-          >
-            {'Datum'}
-          </label>
-          <input
-            className="form-control"
-            id="meetingDate"
-            type="text"
-            placeholder="Datum"
-            {...date}
-          />
-        </fieldset>
-        {' '}
-        <button
-          className="btn btn-primary"
-          type="submit"
-          onClick={handleSubmit}
+  return (
+    <form className="form-inline m-b-1" onSubmit={handleSubmit}>
+      <fieldset className="form-group">
+        <label
+          className="sr-only"
+          htmlFor="meetingDate"
         >
-          {'Dodaj sastanak'}
-        </button>
-      </form>
-    );
-  }
-}
+          {'Datum'}
+        </label>
+        <input
+          className="form-control"
+          id="meetingDate"
+          type="text"
+          placeholder="Datum"
+          {...date}
+        />
+      </fieldset>
+      {' '}
+      <button
+        className="btn btn-primary"
+        type="submit"
+        onClick={handleSubmit}
+      >
+        {'Dodaj sastanak'}
+      </button>
+    </form>
+  );
+};
 
-MeetingForm = reduxForm({
+MeetingForm.propTypes = {
+  fields: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default reduxForm({
   form: 'meeting',
   fields: ['date'],
 })(MeetingForm);
-
-export default MeetingForm;
