f69c3990141f91610b5f1787f5459d0306ac510c
Matija Marohnić <matija.marohnic@gmail.com>
2016-01-26 21:10:13 +0100

    Use library for globally setting jsdom

--- test/global.js
+++ test/global.js
@@ -1,9 +1,5 @@
-const jsdom = require('jsdom').jsdom;
-
 // http://rackt.org/redux/docs/recipes/WritingTests.html#fixing-broken-setstate
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
+require('jsdom-global')();
 
 // avoid Enzyme webpack errors
 // https://github.com/airbnb/enzyme/issues/47#issuecomment-165791879
